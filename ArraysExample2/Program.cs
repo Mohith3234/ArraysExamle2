5af49c576ec00890ca718a3aaf702094ae33f36c
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-20 21:01:01 +0530

    first commit

---
+++ ArraysExample2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ArraysExample2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 emp1 = new Class1() { EmpId=1,Name="Mohith"};
+            Class1 emp2 = new Class1() { EmpId = 2, Name = "Rohith" };
+            Class1 emp3 = new Class1() { EmpId = 3, Name = "Ramesh" };
+            Class1 emp4 = new Class1() { EmpId = 4, Name = "Suresh" };
+
+            Class1[] array= new Class1[] { emp1, emp2, emp3, emp4 };
+            foreach(Class1 obj in array)
+            {
+                Console.WriteLine(obj.EmpId+","+obj.Name);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
